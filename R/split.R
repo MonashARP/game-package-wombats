f5149bcd19ea88576926d9029df3f9aa06540f6a
Weizhu Chen <wche0192@student.monash.edu>
2025-06-04 09:29:53 +1000

    split fuc update with sim fun in the bottom

--- R/split.R
+++ R/split.R
@@ -6,58 +6,107 @@
 #' @return An updated list of lists of blackjack_hand objects after potential splits.
 #' @export
 
-handle_splitting <- function(player_hands, deck, players) {
+handle_splitting <- function8(player_hands, deck, players) {
   updated_player_hands <- player_hands
   deck_index <- 1
   player_names <- names(players)
 
   for (i in seq_along(updated_player_hands)) {
-    hand <- updated_player_hands[[i]][[1]]
-    ph_cards <- vctrs::field(hand, "cards")
-    player_name <- player_names[i]
-    is_computer <- players[[player_name]]$is_computer
+    hand_list <- updated_player_hands[[i]]
+    hand      <- hand_list[[1]]  # assume first hand is the main one
+    player_name <- names_list[i]
+    is_cp <- players[[player_name]]$is_computer
 
-    if (length(ph_cards) == 2 && ph_cards[1] == ph_cards[2]) {
+    if (needs_split(hand)) {
       do_split <- FALSE
-
-      if (is_computer) {
-        # Simple computer logic: always split 8s or Aces
-        if (ph_cards[1] %in% c("8", "A")) {
-          do_split <- TRUE
-          cat(player_name, "decides to split a pair of ", ph_cards[1], "s.\n")
+      if (is_cp) {
+        do_split <- ai_decide_split(hand) # AI logic to decide split
+        if (do_split) {
+          cat(player_name, "decides to split a pair of ",
+              vctrs::field(hand, "rank")[1], "s.\n", sep = "")
         } else {
-          cat(player_name, "decides NOT to split a pair of ", ph_cards[1], "s.\n")
-        }
-      } else {
-        repeat {
-          sp <- tolower(readline(paste0(player_name, ", you have a pair of ", ph_cards[1], "s. Split hand? (yes/no): ")))
-          if (sp %in% c("yes", "no")) {
-            do_split <- (sp == "yes")
-            break
-          }
-          cat("Invalid input. Please enter 'yes' or 'no'.\n")
-        }
+          cat(player_name, "decides NOT to split a pair of ",
+              vctrs::field(hand, "rank")[1], "s.\n", sep = "")
+         }
+        } else {
+        do_split <- ask_human_split(player_name, hand) # Human decision
       }
 
       if (do_split) {
-        first <- new_blackjack_hand(c(ph_cards[1], deck[deck_index]))
-        second <- new_blackjack_hand(c(ph_cards[2], deck[deck_index + 1]))
-        updated_player_hands[[i]] <- list(first, second)
+        res <- perform_split(hand, deck, deck_index)
+        updated_player_hands[[i]] <- res$split_hands
+        deck_index <- res$new_deck_index
+
         cat(player_name, "split into:\n")
         # Hand 1
-        f_cards <- vctrs::field(first, "cards")
-        f_suits <- vctrs::field(first, "suits")
-        f_display <- paste0(f_suits, f_cards)
-        cat("  Hand 1: [", paste(f_display, collapse = " "), "]\n")
+        f1 <- res$split_hands[[1]]
+        f1_r <- vctrs::field(f1, "rank")
+        f1_s <- vctrs::field(f1, "suit")
+        f1_disp <- paste0(f1_s, f1_r)
+        cat("  Hand 1: [", paste(f1_disp, collapse = " "), "]\n", sep = "")
         # Hand 2
-        s_cards <- vctrs::field(second, "cards")
-        s_suits <- vctrs::field(second, "suits")
-        s_display <- paste0(s_suits, s_cards)
-        cat("  Hand 2: [", paste(s_display, collapse = " "), "]\n")
-        deck_index <- deck_index + 2
+        f2 <- res$split_hands[[2]]
+        f2_r <- vctrs::field(f2, "rank")
+        f2_s <- vctrs::field(f2, "suit")
+        f2_disp <- paste0(f2_s, f2_r)
+        cat("  Hand 2: [", paste(f2_disp, collapse = " "), "]\n", sep = "")
       }
     }
   }
 
-  return(updated_player_hands)
+  list(
+    player_hands = updated_player_hands,
+    deck_index   = deck_index
+  )
+}
+
+
+# Check split condition: exactly two cards in the hand with the same point value
+#' @noRd
+needs_split <- function(hand) {
+  ranks <- vctrs::field(hand, "rank")
+  length(ranks) == 2 && ranks[1] == ranks[2]
+}
+
+# AI decision: split if 8 or A in hand, otherwise no split
+#' @noRd
+ai_decide_split <- function(hand, coins = NULL) {
+  ranks <- vctrs::field(hand, "rank")
+  rank <- ranks[1]
+  rank %in% c("8", "A")
+}
+
+# ask human whether to split
+#' @noRd
+ask_human_split <- function(name, hand) {
+  ranks <- vctrs::field(hand, "rank")
+  rank <- ranks[1]
+  repeat {
+    prompt <- paste0(
+      name, ", you have a pair of ", rank,
+      "s. Split hand? (yes/no): "
+    )
+    ans <- tolower(readline(prompt))
+    if (ans %in% c("yes", "no")) {
+      return(ans == "yes")
+    }
+    cat("Invalid input. Please enter 'yes' or 'no'.\n")
+  }
+}
+
+# Perform the split operation
+#' @noRd
+perform_split <- function(hand, deck, deck_index) {
+  # hand: card vsrector of length 2
+  # deck_index: next two cards to draw from the deck
+  first_card  <- hand[1]
+  second_card <- hand[2]
+  new1 <- vctrs::vec_c(first_card, deck[deck_index])
+  new2 <- vctrs::vec_c(second_card, deck[deck_index + 1])
+  hand1 <- new_blackjack_hand(new1)
+  hand2 <- new_blackjack_hand(new2)
+  list(
+    split_hands = list(hand1, hand2),
+    new_deck_index = deck_index + 2
+  )
 }
