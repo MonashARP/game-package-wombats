f75e74c26b9fa17ed6d03fd3700cc92f155489ab
AneeshAgarwala <aaga0022@student.monash.edu>
2025-06-06 02:13:55 +1000

    Adds Rcpp Implementation

--- R/score_cal.R
+++ R/score_cal.R
@@ -1,7 +1,5 @@
-#  R/score.R
-
 #' @title Calculate Blackjack Score
-#' @description Calculates the total point value of a blackjack hand
+#' @description Calculates the total point value of a blackjack hand using C++
 #' @param hand A `blackjack_hand` object
 #' @return Numeric score of the hand (integer)
 #' @export
@@ -13,24 +11,10 @@ calculate_score <- function(hand) {
   cards <- vctrs::field(hand, "cards")
   ranks <- card_rank(cards)
 
-  # Map ranks to numeric values explicitly
-  values <- sapply(ranks, function(rank) {
-    if (rank %in% c("J", "Q", "K")) {
-      10
-    } else if (rank == "A") {
-      11
-    } else {
-      as.numeric(rank)
-    }
-  })
-
-  total <- sum(values)
-  ace_count <- sum(ranks == "A")
-
-  while (total > 21 && ace_count > 0) {
-    total <- total - 10
-    ace_count <- ace_count - 1
-  }
-
-  return(total)
+  # Use C++ implementation
+  return(calculate_score_cpp(ranks))
 }
+
+#' @useDynLib wombat21
+#' @importFrom Rcpp sourceCpp
+NULL
